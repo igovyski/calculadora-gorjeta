aec4abcecfc2105eb4c44a130f50ca80aa86093a
EA - Igor Da Silva Pessoa <pessoa_igor@educadventista.org>
2023-05-23 21:16:04 -0300

    23/05

--- calculadora-gorjeta/src/components/Calculadora.jsx
+++ calculadora-gorjeta/src/components/Calculadora.jsx
@@ -1,8 +1,30 @@
 import './Calculadora.css'
 import cifrao from "../images/cifrao.png"
 import pessoa from "../images/pessoa.png"
+import { useState } from 'react'
 
 export default function Calculadora(){
+
+    const [conta, setConta] = useState(0)
+    const {pessoas, setPessoas} = useState(0)
+    const {porcentagem, setPorcentagem} = useState(0)
+
+    function handleAlterarConta(event) {
+        setConta(Number(event.target.value))
+    }
+
+    function handleAlterarPessoas(event) {
+        setPessoas(Number(event.target.value))
+    }
+
+    function handleAlterarPorcentagem(event, numero) {
+        if (event === 0) {
+            return setPorcentagem(numero)
+        }
+
+        return setPorcentagem(Number(event.target.value))
+    }
+
     return(
         <main>
             <form action="">
@@ -10,25 +32,25 @@ export default function Calculadora(){
                     <label htmlFor="bill">Conta</label>
                     <div className="input-box">
                         <img src={cifrao} alt="Cifrão" />
-                        <input id="bill" placeholder="0" type="number" />
+                        <input id="bill" placeholder="0" type="number" onChange={handleAlterarConta}/>
                     </div>
                 </div>
                 <div className="tip-percentage mb-div">
                     <label htmlFor="custom">Selecione a Porcentagem</label>
                     <div>
-                        <input type="button" value="5%" />
-                        <input type="button" value="10%" />
-                        <input type="button" value="15%" />
-                        <input type="button" value="25%" />
-                        <input type="button" value="50%" />
-                        <input type="number" id="custom" placeholder="Outra" />
+                        <input type="button" value="5%" onClick={ () => handleAlterarPorcentagem(0, 5)} className={porcentagem === 5 ? "selected" : ""} />
+                        <input type="button" value="10%" onClick={ () => handleAlterarPorcentagem(0, 10)} className={porcentagem === 10 ? "selected" : ""} />
+                        <input type="button" value="15%" onClick={ () => handleAlterarPorcentagem(0, 15)} className={porcentagem === 15 ? "selected" : ""} />
+                        <input type="button" value="25%" onClick={ () => handleAlterarPorcentagem(0, 25)} className={porcentagem === 25 ? "selected" : ""} />
+                        <input type="button" value="50%" onClick={ () => handleAlterarPorcentagem(0, 50)} className={porcentagem === 50 ? "selected" : ""} />
+                        <input type="number" id="custom" placeholder="Outra" onChange={handleAlterarPorcentagem} />
                     </div>
                 </div>
                 <div className="people mb-div">
                     <label htmlFor="people-number">Número de Pessoas</label>
                     <div className="input-box">
                         <img src={pessoa} alt="Pessoa" />
-                        <input type="number" id="people-number" placeholder="0"/>
+                        <input type="number" id="people-number" placeholder="0" onChange={handleAlterarPessoas} />
                     </div>
                 </div>
             </form>
