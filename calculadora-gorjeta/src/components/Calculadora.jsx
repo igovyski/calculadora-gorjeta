6d555543efb91f0cc4a3c08993dffde2f158d29f
EA - Igor Da Silva Pessoa <pessoa_igor@educadventista.org>
2023-05-30 20:24:16 -0300

    TERMINEI ESSA PORRA DO CARALHO

--- calculadora-gorjeta/src/components/Calculadora.jsx
+++ calculadora-gorjeta/src/components/Calculadora.jsx
@@ -6,8 +6,8 @@ import { useState } from 'react'
 export default function Calculadora(){
 
     const [conta, setConta] = useState(0)
-    const {pessoas, setPessoas} = useState(0)
-    const {porcentagem, setPorcentagem} = useState(0)
+    const [pessoas, setPessoas]  = useState(0)
+    const [porcentagem, setPorcentagem] = useState(0)
 
     function handleAlterarConta(event) {
         setConta(Number(event.target.value))
@@ -26,9 +26,9 @@ export default function Calculadora(){
     }
 
     function handleLimpar() {
-        setConta = 0
-        setPessoas = 0
-        setPorcentagem = 0
+        setConta(0)
+        setPessoas(0)
+        setPorcentagem(0)
     }
 
     let gorjeta = 0
@@ -47,7 +47,7 @@ export default function Calculadora(){
                     <label htmlFor="bill">Conta</label>
                     <div className="input-box">
                         <img src={cifrao} alt="Cifrão" />
-                        <input id="bill" placeholder="0" type="number" onChange={handleAlterarConta}/>
+                        <input id="bill" placeholder="0" type="number" onChange={handleAlterarConta} value={conta === 0 ? " " : conta}/>
                     </div>
                 </div>
                 <div className="tip-percentage mb-div">
@@ -58,29 +58,31 @@ export default function Calculadora(){
                         <input type="button" value="15%" onClick={ () => handleAlterarPorcentagem(0, 15)} className={porcentagem === 15 ? "selected" : ""} />
                         <input type="button" value="25%" onClick={ () => handleAlterarPorcentagem(0, 25)} className={porcentagem === 25 ? "selected" : ""} />
                         <input type="button" value="50%" onClick={ () => handleAlterarPorcentagem(0, 50)} className={porcentagem === 50 ? "selected" : ""} />
-                        <input type="number" id="custom" placeholder="Outra" onChange={handleAlterarPorcentagem} />
+                        <input type="number" id="custom" placeholder="Outra" onChange={handleAlterarPorcentagem} value={porcentagem === 0 ? " " : porcentagem} />
                     </div>
                 </div>
                 <div className="people mb-div">
                     <label htmlFor="people-number">Número de Pessoas</label>
                     <div className="input-box">
                         <img src={pessoa} alt="Pessoa" />
-                        <input type="number" id="people-number" placeholder="0" onChange={handleAlterarPessoas} />
+                        <input type="number" id="people-number" placeholder="0" onChange={handleAlterarPessoas} value={pessoas === 0 ? " " : pessoas}/>
                     </div>
                 </div>
             </form>
             <section className="results">
-                <div className="tip-amount">
-                    <p>Gorjeta</p><br />
-                    <span>/ pessoa</span>
-                    <p>R$ {gorjeta.toFixed(2)}</p>
+                <div>
+                    <div className="tip-amount">
+                        <p>Gorjeta</p><br />
+                        <span>/ pessoa</span>
+                        <p>R$ {gorjeta.toFixed(2)}</p>
+                    </div>
+                    <div className="total">
+                        <p>Total</p><br />
+                        <span>/ pessoa</span>
+                        <p>R$ {total.toFixed(2)}</p>
+                    </div>
                 </div>
-                <div className="total">
-                    <p>Total</p><br />
-                    <span>/ pessoa</span>
-                    <p>R$ {total.toFixed(2)}</p>
-                </div>
-                <button>Limpar</button>
+                <button onClick={handleLimpar}>Limpar</button>
             </section>
         </main>
     )
